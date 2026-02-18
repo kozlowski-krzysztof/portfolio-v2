f0d14b07a39bb16d375a774eb411aacf2c569c21
Krzysztof Kozlowski <kozlowski@krzysztof.se>
2026-02-18 08:32:46 +0000

    changed astro config to work with devcontainers + vite-bundle-analyzer

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,25 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react()],
+	server: {
+		port: 4321,
+		host: (() => {
+			if (process.env.IN_DEVCONTAINER === "true") {
+				return true;
+			} else {
+				return false;
+			}
+		})(),
+	},
+
+	vite: {
+		plugins: [analyzer()],
+		server: {
+			watch: { usePolling: true },
+		},
+	},
+});
